1444a37b17af4758abe4e1691ac1d35210afb5d4
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-07 18:11:46 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,74 @@
-<?php
-echo ('footer partial');
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row row-cols-2">
+                <div class="col-7 py-3">
+                    <div class="row row-cols-3">
+                        <div class="col">
+                            <h4>DC COMICS</h4>
+                            <ul class="list-unstyled">
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h4>SHOP</h4>
+                            <ul class="list-unstyled">
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h4>DC</h4>
+                            <ul class="list-unstyled">
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy (New)</li>
+                                <li>Ad Choise</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h4>SITES</h4>
+                            <ul class="list-unstyled">
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p>
+                        All SitesContent TM and C 2020 DC Entertainment, unles otherwise <span class="text-primary">noted here</span>. All rights reserved.
+                        <br>
+                        <span class="text-primary">Cookies Settings</span>
+                    </p>
+                </div>
+                <div class="col-5 footer-image"></div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom bg-dark">
+        <div class="container d-flex justify-content-between py-3">
+            <button class="btn border-primary rounded-0 text-white">SIGN-UP NOW!</button>
+            <div class="socials g-4">
+                <span class="text-primary">FOLLOW US</span>
+                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-facebook.png')}}" alt="">
+                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-twitter.png')}}" alt="">
+                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-youtube.png')}}" alt="">
+                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-pinterest.png')}}" alt="">
+                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-periscope.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
