8126a30231ae5f16ed619b31f445afcf9e9c7eb9
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-07 16:33:09 +0200

    add basic structure and assets

---
+++ resources/views/single.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('page-title', 'single')
+
+@section('content')
+<div>main single placeholder</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guests.home');
+})->name('guests.home');
+
+Route::get('/single', function () {
+    return view('guests.single');
+})->name('guests.single');
