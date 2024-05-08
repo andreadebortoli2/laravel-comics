7ee94e33f1db76fa698ca129f782a16d78f1f0df
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-08 13:20:12 +0200

    edit comics.index main-top style with sass

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,20 +3,20 @@
 @section('page-title', 'home')
 
 @section('content')
-    <div class="main-top bg-dark">
-        <div class="container" style="position: relative;">
-            <h2 class="bg-primary p-3" style="width: fit-content; position: absolute; top: -5%">CURRENT SERIES</h2>
-            <div class="container py-3">
-                <div class="row row-cols-6 pt-5">
+    <div class="main-top">
+        <div class="container">
+            <h2>CURRENT SERIES</h2>
+            <div class="container">
+                <div class="row row-cols-6 g-4">
                     @foreach ($comics as $key => $comic)
                         <div class="col">
                             <a href="{{ route('comics.show', ['id' => $key]) }}">
-                                <div class="card border-0 rounded-0 bg-transparent height-50">
+                                <div class="card border-0 rounded-0">
                                     <img class="card-img-top rounded-0" src="{{ $comic['thumb'] }}"
-                                        alt="cover of {{ $comic['title'] }}" style="height: 200px; width: 200px;" />
+                                        alt="cover of {{ $comic['title'] }}" />
                                     <div class="card-body px-0">
                                         <h6 class="card-title text-white">
-                                            {{ str_contains($comic['title'], '#') ? stristr($comic['title'], '#', true) : $comic['title'] }}
+                                            {{ strtoupper(str_contains($comic['title'], '#') ? stristr($comic['title'], '#', true) : $comic['title']) }}
                                         </h6>
                                     </div>
                                 </div>
@@ -24,13 +24,11 @@
                         </div>
                     @endforeach
                 </div>
-            </div>
-            <div class="text-center pb-5">
-                <button class="btn bg-primary text-white">LOAD MORE</button>
+                <button class="btn rounded-0">LOAD MORE</button>
             </div>
         </div>
     </div>
-    <div class="mainbottom bg-primary text-white">
+    <div class="main-bottom bg-primary text-white">
         <div class="container d-flex justify-content-between">
             <div class="d-flex align-items-center">
                 <img src="{{ Vite::asset('resources/images/img/buy-comics-digital-comics.png') }}" alt=""
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel @yield('page-title', 'comics')</title>
+    <title>Laravel - @yield('page-title', 'comics')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
