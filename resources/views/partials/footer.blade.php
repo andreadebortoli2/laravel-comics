57bd7eed9b4362f4504829b47d433f323a3e6425
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-08 19:02:22 +0200

    edit footer style with sass

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="footer-top">
-        <div class="container">
+        <div class="wrapper">
             <div class="row row-cols-2">
-                <div class="col-7 py-3">
+                <div class="col-7 footer-nav">
                     <div class="row row-cols-3">
                         <div class="col">
                             <h4>DC COMICS</h4>
@@ -49,25 +49,26 @@
                         </div>
                     </div>
                     <p>
-                        All SitesContent TM and C 2020 DC Entertainment, unles otherwise <span class="text-primary">noted here</span>. All rights reserved.
+                        All SitesContent TM and C 2020 DC Entertainment, unles otherwise <span>noted here</span>. All
+                        rights reserved.
                         <br>
-                        <span class="text-primary">Cookies Settings</span>
+                        <span>Cookies Settings</span>
                     </p>
                 </div>
                 <div class="col-5 footer-image"></div>
             </div>
         </div>
     </div>
-    <div class="footer-bottom bg-dark">
-        <div class="container d-flex justify-content-between py-3">
-            <button class="btn border-primary rounded-0 text-white">SIGN-UP NOW!</button>
-            <div class="socials g-4">
-                <span class="text-primary">FOLLOW US</span>
-                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-facebook.png')}}" alt="">
-                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-twitter.png')}}" alt="">
-                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-youtube.png')}}" alt="">
-                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-pinterest.png')}}" alt="">
-                <img class="mx-2" src="{{Vite::asset('resources/images/img/footer-periscope.png')}}" alt="">
+    <div class="footer-bottom">
+        <div class="wrapper">
+            <button class="btn">SIGN-UP NOW!</button>
+            <div class="socials">
+                <span>FOLLOW US</span>
+                <img src="{{ Vite::asset('resources/images/img/footer-facebook.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/images/img/footer-twitter.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/images/img/footer-youtube.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/images/img/footer-pinterest.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/images/img/footer-periscope.png') }}" alt="">
             </div>
         </div>
     </div>
