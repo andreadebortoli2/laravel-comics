04b0f2ab8d786cd434fb1317befee43c40f3f1c9
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-08 17:09:47 +0200

    edit comics.show style with sass

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,103 +4,117 @@
 
 @section('content')
 
-    <div class="comic-cover bg-primary" style="height: 75px; position:relative;">
+    <div class="comic-cover">
         <div class="container">
-            <div class="card rounded-0" style="position: absolute; bottom: 20px">
-                <img class="card-img-top rounded-0" src="{{ $comic['thumb'] }}" alt="Title" style="height: 200px;" />
+            <div class="card rounded-0">
+                <img class="card-img-top rounded-0" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" />
                 <div class=" card-body p-0">
-                    <h4 class="card-title p-1 bg-info text-white"
-                        style="width: fit-content; position: absolute; top: 0; left: 0; font-size: 0.8rem">
+                    <h4 class="card-title">
                         COMIC-BOOK</h4>
-                    <p class="card-text p-1 text-center bg-info text-white">VIEW GALLERY</p>
+                    <p class="card-text">VIEW GALLERY</p>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-8">
-                <h2 class="text-primary pt-4">
-                    {{ strtoupper($comic['title']) }}
-                </h2>
-                <div class="row my-3 bg-success-subtle border-1 border-bottom border-secondary">
-                    <div class="col-9 left-info d-flex justify-content-between align-items-center">
-                        <div class="text-success">
-                            U.S. Price: <span class="text-white">{{ $comic['price'] }}</span>
-                        </div>
-                        <div class="availability text-success pe-4">
-                            AVAILABLE
-                        </div>
-                    </div>
-                    <div class="col-3 right-info border-1 border-start border-secondary">
-                        <div class="dropdown text-center">
-                            <button class="btn text-white dropdown-toggle" type="button" id="triggerId"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Check Availability
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="triggerId">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item disabled" href="#">Disabled action</a>
-                                <h6 class="dropdown-header">Section header</h6>
-                                <a class="dropdown-item" href="#">Action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">After divider action</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <p>
-                        {{ $comic['description'] }}
-                    </p>
-                </div>
-            </div>
-            <div class="col-4 px-4">
-                <h4 class="text-secondary text-end">ADVERTISEMENT</h4>
-                <img src="{{ Vite::asset('resources/images/img/adv.jpg') }}" alt="" style="width: 100%;">
             </div>
         </div>
     </div>
 
-    <div class="comic-info bg-light border-1 border-top border-bottom border-secondary">
+    <div class="comic-info-top">
         <div class="container">
-            <div class="row row-cols-2 py-5">
+            <div class="row">
+                <div class="col-8">
+                    <h2>
+                        {{ strtoupper($comic['title']) }}
+                    </h2>
+                    <div class="quick-info">
+                        <div class="col-9 left-info">
+                            <div>
+                                U.S. Price: <span>{{ $comic['price'] }}</span>
+                            </div>
+                            <div class="availability">
+                                AVAILABLE
+                            </div>
+                        </div>
+                        <div class="col-3 right-info">
+                            <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    Check Availability
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="triggerId">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item disabled" href="#">Disabled action</a>
+                                    <h6 class="dropdown-header">Section header</h6>
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">After divider action</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p class="comic-description">
+                            {{ $comic['description'] }}
+                        </p>
+                    </div>
+                </div>
+                <div class="advertisement col-4">
+                    <h4>ADVERTISEMENT</h4>
+                    <img src="{{ Vite::asset('resources/images/img/adv.jpg') }}"
+                        alt="advertisement image about DC power visa">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="comic-info-bottom">
+        <div class="container">
+            <div class="row row-cols-2 g-5">
                 <div class="col">
-                    <h3 class="pb-5">Talent</h3>
-                    <div class="artist d-flex border-1 border-top border-secondary py-3">
+                    <h3>Talent</h3>
+                    <div class="artist">
                         <div class="col-3">
                             <h5>Art by:</h5>
                         </div>
                         <div class="col-9">
-                            <p class="text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-                                ipsa necessitatibus possimus, tenetur ipsum nesciunt voluptatum harum consequuntur
-                                perspiciatis illo excepturi, quibusdam explicabo ab adipisci quisquam modi atque?
-                                Tenetur, debitis.</p>
+                            <p>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                                    ipsa necessitatibus possimus, tenetur ipsum nesciunt voluptatum harum consequuntur
+                                    perspiciatis illo excepturi, quibusdam explicabo ab adipisci quisquam modi atque?
+                                    Tenetur, debitis.
+                                </span>
+                            </p>
                         </div>
                     </div>
-                    <div class="writer d-flex border-1 border-top border-bottom border-secondary py-3">
+                    <div class="writer">
                         <div class="col-3">
                             <h5>Written by:</h5>
                         </div>
                         <div class="col-9">
-                            <p class="text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                                eius laboriosam vero fugit, mollitia voluptates ea maxime nostrum omnis? Mollitia
-                                necessitatibus hic architecto repudiandae eaque aperiam temporibus quaerat fuga
-                                voluptatum.</p>
+                            <p>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                                    eius laboriosam vero fugit, mollitia voluptates ea maxime nostrum omnis? Mollitia
+                                    necessitatibus hic architecto repudiandae eaque aperiam temporibus quaerat fuga
+                                    voluptatum.
+                                </span>
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <h3 class="pb-5">Specs</h3>
-                    <div class="series d-flex border-1 border-top border-secondary py-3">
+                    <h3>Specs</h3>
+                    <div class="series">
                         <div class="col-3">
                             <h5>Series:</h5>
                         </div>
                         <div class="col-9">
-                            <p class="text-primary">{{ strtoupper($comic['series']) }}</p>
+                            <p><span>
+                                    {{ strtoupper($comic['series']) }}
+                                </span>
+                            </p>
                         </div>
                     </div>
-                    <div class="price d-flex border-1 border-top border-secondary py-3">
+                    <div class="price">
                         <div class="col-3">
                             <h5>U.S. Price:</h5>
                         </div>
@@ -108,7 +122,7 @@
                             <p>{{ $comic['price'] }}</p>
                         </div>
                     </div>
-                    <div class="sale-date d-flex border-1 border-top border-bottom border-secondary py-3">
+                    <div class="sale-date">
                         <div class="col-3">
                             <h5>On Sale Date:</h5>
                         </div>
@@ -117,33 +131,28 @@
                         </div>
                     </div>
                 </div>
-                <div class="col"></div>
             </div>
         </div>
     </div>
 
-    <div class="utilities border-1 border-bottom border-secondary bg-light">
+    <div class="utilities">
         <div class="container">
             <div class="row row-cols-4">
-                <div class="col d-flex justify-content-between pt-3 border-1 border-start border-secondary">
+                <div class="col">
                     <h4>DIGITAL COMICS</h4>
-                    <img src="{{ Vite::asset('resources/images/img/buy-comics-digital-comics.png') }}" alt=""
-                        style="background-color:gray; height:50%; padding: 0 1rem;">
+                    <img src="{{ Vite::asset('resources/images/img/buy-comics-digital-comics.png') }}" alt="">
                 </div>
-                <div class="col d-flex justify-content-between pt-3 border-1 border-start border-secondary">
+                <div class="col">
                     <h4>SHOP DC</h4>
-                    <img src="{{ Vite::asset('resources/images/img/buy-comics-subscriptions.png') }}" alt=""
-                        style="background-color:gray; height:50%; padding: 0 1rem;">
+                    <img src="{{ Vite::asset('resources/images/img/buy-comics-subscriptions.png') }}" alt="">
                 </div>
-                <div class="col d-flex justify-content-between pt-3 border-1 border-start border-secondary">
+                <div class="col">
                     <h4>COMIC SHOP LOCATOR</h4>
-                    <img src="{{ Vite::asset('resources/images/img/buy-comics-shop-locator.png') }}" alt=""
-                        style="background-color:gray; height:50%; padding: 0 1rem;">
+                    <img src="{{ Vite::asset('resources/images/img/buy-comics-shop-locator.png') }}" alt="">
                 </div>
-                <div class="col d-flex justify-content-between pt-3 border-1 border-start border-end border-secondary">
+                <div class="col">
                     <h4>SUBSCRPTIONS</h4>
-                    <img src="{{ Vite::asset('resources/images/img/buy-comics-merchandise.png') }}" alt=""
-                        style="background-color:gray; height:50%; padding: 0 1rem;">
+                    <img src="{{ Vite::asset('resources/images/img/buy-comics-merchandise.png') }}" alt="">
                 </div>
             </div>
         </div>
